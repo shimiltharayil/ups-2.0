c48a7b723eea808a80599e61dd0a235997a37aae
shimiltharayil <shimiltharayil007@gmail.com>
2023-03-27 20:18:31 +0530

    adding react-navigation

---
+++ screens/CustomersScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+
+
+const CustomersScreen = ( ) => {
+  return (
+    <View>
+      <Text className="">CustomersScreen</Text>
+    </View>
+  );
+}
+
+export default CustomersScreen
