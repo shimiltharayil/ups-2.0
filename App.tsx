5167786e99576e491aab24649c6e218347b60df9
shimiltharayil <shimiltharayil007@gmail.com>
2023-03-29 12:47:38 +0530

    graphql added

--- App.tsx
+++ App.tsx
@@ -2,12 +2,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 import CustomersScreen from "./screens/CustomersScreen";
 import RootNavigator from "./navigator/RootNavigator";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+const client = new ApolloClient({
+  uri: "http://localhost:5001/api/ordered-sponge",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore className
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
